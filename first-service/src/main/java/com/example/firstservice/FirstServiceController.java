1b5251736a8ac5dd0d6566943b781c925b981c87
xognstl <xognstl@naver.com>
2024-12-09 16:53:26 +0900

    20241209 16:53 commit

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,14 +1,23 @@
 package com.example.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service")
 public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "welcome to the First service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        log.info(header);
+        return "hello world in First Service";
     }
 }
--- zuul-service/src/main/java/com/example/zuulservice/filter/ZuulLoggingFilter.java
+++ zuul-service/src/main/java/com/example/zuulservice/filter/ZuulLoggingFilter.java
@@ -1,2 +1,47 @@
-package com.example.zuulservice.filter;public class ZuulLoggingFilter {
+package com.example.zuulservice.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Slf4j
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+//    Logger logger = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+    // lombok @Slf4j
+
+    @Override
+    public Object run() throws ZuulException {  //실제동작
+        log.info("************************ printing logs : ");
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest(); //servlet 에서 사용자 요청 정보 처리
+        log.info("************************ " + request.getRequestURI());
+
+        return null;
+    }
+
+    @Override
+    public String filterType() {
+        return "pre";   // 사전 필터 (사후는 post)
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;   // 순서
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;    //사용 여부
+    }
+
+
 }
